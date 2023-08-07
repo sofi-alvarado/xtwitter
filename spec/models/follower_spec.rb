6466396ce4a58960bb209bf61b4ac5c07e844d25
Rene Garcia <rgarcia@ioogo.com>
2023-08-07 16:01:27 +0000

    Adding validations for follower model

--- spec/models/follower_spec.rb
+++ spec/models/follower_spec.rb
@@ -5,4 +5,10 @@ RSpec.describe Follower, type: :model do
     it { should belong_to(:follower).class_name("User").with_foreign_key(:follower_id) } 
     it { should belong_to(:followee).class_name("User").with_foreign_key(:followee_id) } 
   end
+
+  context "validations" do
+    it { should validate_presence_of(:follower_id) }
+    it { should validate_presence_of(:followee_id) }
+    it { should validate_uniqueness_of(:follower_id).scoped_to(:followee_id) }
+  end
 end
