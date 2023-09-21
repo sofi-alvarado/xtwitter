2d9f3d04cc88c002b0746c2ebf1536770c3168c7
sofia <sofiaalvaradohenriquez@gmail.com>
2023-09-21 15:22:37 -0600

    Adding Like validations

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,4 +1,7 @@
 class Bookmark < ApplicationRecord
   belongs_to :tweet
   belongs_to :user
+
+  #add association validations to your model
+  validates_associated :tweet, :user
 end
--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,13 @@
 class Like < ApplicationRecord
   belongs_to :tweet
   belongs_to :user
+
+  #add association validations to your model
+  validates_associated :tweet, :user
+
+  #User like
+  validates: user_id, uniqueness: true
+  #Liked tweet
+  validates: tweet_id, uniqueness: true
+
 end
