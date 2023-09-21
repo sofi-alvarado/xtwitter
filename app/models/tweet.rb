560e21c4664601556f967a912116ce65d8477e13
sofia <sofiaalvaradohenriquez@gmail.com>
2023-09-21 15:40:38 -0600

    Tweet validation, if its retweet

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -2,20 +2,19 @@ class Tweet < ApplicationRecord
   belongs_to :user
   #self reference
   belongs_to :retweet, class_name: "Tweet", optional: true
-  #a Tweet can have many replies
-  has_many :replies
   #a bookmark belongs to a single Tweet
   has_one :bookmarks
-  #a Tweet can have many likes
-  has_many :likes
-  #a Tweet can have many taggins
-  has_many :taggings
   has_and_belongs_to_many :hashtags, through: :taggings
 
   #Validating if content exists and if in a length betweent 1 and 255
   #If content is not provided throw message: is too short (minimum is 1 character)", "Please enter your text"
   validates :content, length: { within:(1...255)}, presence: { message: "Please enter your text" }
+
+  validates :content, presence: true, if: :uniqueness_tweet_id?
   
+  def uniqueness_tweet_id?
+    :retweet_id.nil?
+  end
   
 end
 
