192341cc4c91132452a5803bcec153717ac557f2
sofia <sofiaalvaradohenriquez@gmail.com>
2023-09-21 11:24:27 -0600

    Adding Replies Validations

--- app/models/reply.rb
+++ app/models/reply.rb
@@ -2,4 +2,10 @@ class Reply < ApplicationRecord
   #Associations with table tweets and users
   belongs_to :tweet
   belongs_to :user
+
+  #Validating if content exists and if is in a length betweent 1 and 255
+  validates :content, length: { within:(1...255)}, presence: true
+  #Validates if the content of the reply exists
+  validates :quote, presence: true
+  
 end
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -12,6 +12,14 @@ class Tweet < ApplicationRecord
   has_many :taggings
   has_and_belongs_to_many :hashtags, through: :taggings
 
-  validates :content, length: { maximum: 255}, presence: true
+  #Validating if content exists and if in a length betweent 1 and 255
+  validates :content, length: { within:(1...255)}, presence: true
+  
+  
 end
+
+#validates :legacy_code, format: {with: /regex/}
+#presence validates the attribute is not nil, or if its present
+#absence the opposite of presence
+#validates :email, uniqueness: true
  
