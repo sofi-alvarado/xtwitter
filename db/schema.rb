1a0df025eb630c7dcf76e071ab24c6e1a1eaff07
sofia <sofiaalvaradohenriquez@gmail.com>
2023-09-20 19:17:37 -0600

    Adding Tweets Arrat at Hashtags table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_21_010641) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_21_011451) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -34,6 +34,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_21_010641) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "tweets", array: true
   end
 
   create_table "likes", force: :cascade do |t|
