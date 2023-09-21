a4883db6db03450b71daca4a70d04ccffb6e3ecd
sofia <sofiaalvaradohenriquez@gmail.com>
2023-09-21 15:15:32 -0600

    Adding Follow, Hashtag & User validations

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -2,4 +2,8 @@ class Follow < ApplicationRecord
     #name of the association, class name: where it belongs
     belongs_to :following_user, class_name: "User" , foreign_key: "following_user_id"
     belongs_to :follower_user, class_name: "User" , foreign_key: "follower_user_id"
+
+    #Add uniqueness validation for tweet_id and user_id
+    validates :follower_user_id, uniqueness { scope: :following_user_id }
+    validates_associated :follower_user, :following_user
 end
--- app/models/hashtag.rb
+++ app/models/hashtag.rb
@@ -1,4 +1,7 @@
 class Hashtag < ApplicationRecord
     has_many :taggings
     has_many :tweets, through: :taggings
+
+    #Add presence validation for a hashtag name
+    validates :name, presence: true
 end
