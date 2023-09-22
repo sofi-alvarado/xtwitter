e4c679c98bb8512026b37d8a4d4257880411bd07
sofia <sofiaalvaradohenriquez@gmail.com>
2023-09-22 11:50:29 -0600

    Like validation working

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,19 +3,19 @@ class User < ApplicationRecord
     has_many :follows_as_following, class_name: "Follow", foreign_key: "following_user_id"
     has_many :follows_as_follower, class_name: "Follow", foreign_key: "follower_user_id"
     #an user can have many tweets 
-    has_many :tweets
+    has_many :tweets, inverse_of: :user
     #an user can have many replies
     has_many :replies
     #an user can have many bookmarks
     has_many :bookmarks
-    #a like belongs to an unique user
-    has_one :likes
-
+    #a user can like many tweets
+    has_many :likes, inverse_of: :user
     #name will be treated as the displaying name in Twitter
     #I will take lastname as an optional value 
     validates :name, :username, :email, :username, :password, presence: true
     #Add Uniqueness validation for email and username
-    validates :username, uniqueness: true, presence: { message: "Please enter your text" }
+    #no record with the same user or email wil be created
+    validates :username, :email, uniqueness: true, presence: true
     #Add Length validation of 12 characters minimum for password
     validates :password, length: { minimum: 12}, format: { with: /(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[!@#$%^&*()_+])/ }
     
