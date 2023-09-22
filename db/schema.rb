69c79701af8e03141b23489aa5987ba7de5f7cc6
sofia <sofiaalvaradohenriquez@gmail.com>
2023-09-22 13:22:13 -0600

    Adding user_id to table Follows

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_21_153706) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_22_191251) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -28,6 +28,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_21_153706) do
     t.datetime "updated_at", null: false
     t.integer "following_user_id"
     t.integer "follower_user_id"
+    t.integer "user_id"
   end
 
   create_table "hashtags", force: :cascade do |t|
@@ -92,6 +93,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_21_153706) do
 
   add_foreign_key "bookmarks", "tweets"
   add_foreign_key "bookmarks", "users"
+  add_foreign_key "follows", "users"
   add_foreign_key "follows", "users", column: "follower_user_id"
   add_foreign_key "follows", "users", column: "following_user_id"
   add_foreign_key "likes", "tweets"
