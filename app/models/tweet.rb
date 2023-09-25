7eb6f1ac92454f2a505aedef46e7878303427dc0
Sofía Alvarado <72232312+sofi-alvarado@users.noreply.github.com>
2023-09-25 07:59:42 -0600

    Update app/models/tweet.rb
    Co-authored-by: René Alfredo García Hernández <rene.garciah90@gmail.com>

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -8,9 +8,9 @@ class Tweet < ApplicationRecord
 
   #Validating if content exists and if in a length betweent 1 and 255
   #If content is not provided throw message: is too short (minimum is 1 character)", "Please enter your text"
-  validates :content, length: { within:(1...255)}, presence: { message: "Please enter your text" }
+  validates :content, length: { within:(1...255)}
 
-  validates :content, presence: true, if: :uniqueness_tweet_id?
+  validates :content, presence: { message: "Please enter your text" }, if: :uniqueness_tweet_id?
   
   def uniqueness_tweet_id?
     :retweet_id.nil?
