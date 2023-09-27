d5875aa915453fcf32314c37c52b238c3bb7822a
sofia <sofiaalvaradohenriquez@gmail.com>
2023-09-27 15:58:45 -0600

    Reply associations

--- app/models/reply.rb
+++ app/models/reply.rb
@@ -4,7 +4,7 @@ class Reply < ApplicationRecord
   belongs_to :users, class_name: "User", foreign_key: :user_id
   # Validating if content exists and if is in a length betweent 1 and 255
   # If content is not provided throw message: is too short (minimum is 1 character)", "Please enter your text"
-  validates :quote, length: { within:(1...255)}, presence: { message: "Please enter your text" }
+  validates :quote, length: { maximum: 255 }, presence: { message: "Please enter your text" }
   # Retrieve replies from a tweet
   scope :replies_tweet , -> (tweet_id) { where(tweet_id: tweet_id) }
 end
--- spec/models/reply_spec.rb
+++ spec/models/reply_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Reply, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+    it { should validate_length_of(:quote).is_at_most(255) }
+    it { should validate_presence_of(:quote).with_message("Please enter your text") }
+  end
+
+  describe "associations" do
+    it { should belong_to(:tweets).class_name("Tweet").with_foreign_key(:tweet_id)  }
+    it { should belong_to(:users).class_name("User").with_foreign_key(:user_id) }
+  end
+
 end
