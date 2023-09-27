6a0ff5a7f96f56f68657e6e1c6a2671c0d8919e3
sofia <sofiaalvaradohenriquez@gmail.com>
2023-09-27 09:27:45 -0600

    Adding create_replies method

--- app/models/reply.rb
+++ app/models/reply.rb
@@ -1,15 +1,10 @@
 class Reply < ApplicationRecord
-  #Associations with table tweets and users
-  belongs_to :tweet
-  belongs_to :user
-  #add association validations to your model
-  validates_associated :user, :tweet
-  #Validating if content exists and if is in a length betweent 1 and 255
-   #If content is not provided throw message: is too short (minimum is 1 character)", "Please enter your text"
+  # Associations with table tweets and users
+  belongs_to :tweets, class_name: "Tweet", foreign_key: :tweet_id
+  belongs_to :users, class_name: "User", foreign_key: :user_id
+  # Validating if content exists and if is in a length betweent 1 and 255
+  # If content is not provided throw message: is too short (minimum is 1 character)", "Please enter your text"
   validates :quote, length: { within:(1...255)}, presence: { message: "Please enter your text" }
-
-  #retrieve replies from a tweet
-  scope :replies_tweet , -> (tweet_id) { where(tweet: tweet_id) }
-
-
+  # Retrieve replies from a tweet
+  scope :replies_tweet , -> (tweet_id) { where(tweet_id: tweet_id) }
 end
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,33 +1,50 @@
 class Tweet < ApplicationRecord
   belongs_to :user
-  #self references
+  # Self references
   has_many :retweets, class_name: "Tweet", foreign_key: :retweet_id
   has_many :quotes, class_name: "Tweet", foreign_key: :quote_id
-  #a bookmark belongs to a single Tweet
+  # A bookmark belongs to a single Tweet
   has_many :bookmarks
   has_many :hashtags
   has_many :likes
-  #Validating if content exists and if in a length betweent 1 and 255
-  #If content is not provided throw message: is too short (minimum is 1 character)", "Please enter your text"
+  has_many :replies, class_name: "Reply"
+  # Validating if content exists and if in a length betweent 1 and 255
+  # If content is not provided throw message: is too short (minimum is 1 character)", "Please enter your text"
   validates :content, length: { within:(1...255)}, presence: { message: "Please enter your text" }
   validates :quote, length: { maximum: 225 }
   validates :content, presence: true
   validates_associated :user, :retweets
 
-  #Retweets counts: Create a new scope that retrieves the number of retweets
+  # Retweets counts: Create a new scope that retrieves the number of retweets
   scope :retweets_count, -> (tweet_id){ where(retweet_id: tweet_id).count }
+  # Retweets counts: Create a new scope that retrieves the number of retweets
   scope :quotes_count, -> (tweet_id){ where(quote_id: tweet_id).count }
   
-  #Retweet method: Create a method that encapsulates the retweet logic accepting a user a parameter
+  # Retweet method: Create a method that encapsulates the retweet logic accepting a user a parameter
   def create_retweet(user_id)
     original_tweet = Tweet.find(self.id) # Find the original tweet
     retweet_content = original_tweet.content # Access the content of the original tweet
     retweet = Tweet.create(user_id: user_id, content: retweet_content, retweet_id: self.id)
   end
-  #Create a method that encapsulates the retweet logic accepting a user an a text body as parameter
+
+  # Create a method that encapsulates the retweet logic accepting a user an a text body as parameter
   def create_quoted_retweet(user_id, quote)
     original_tweet = Tweet.find(self.id) # Find the original tweet
     tweet_content = original_tweet.content # Access the content of the original tweet
-    quoted_retweet = Tweet.create(user_id: user_id, content: tweet_content, quote_id: self.id, quote: quote)
+    quoted = Tweet.create(user_id: user_id, content: tweet_content, quote_id: self.id, quote: quote)
   end
+
+  # Creating replies
+  def create_reply(user_id, reply)
+    original_tweet = Tweet.find(self.id) # Find the original tweet
+    tweet_reply = Reply.create(tweet_id: self.id, user_id: user_id, quote: reply)
+  end
+
+  # Like a tweet: Create a method that encapsulates the like logic accepting a user
+  def liked_tweet(user_id)
+    tweet = Tweet.find(self.id) # Finding the tweet
+    liked = Like.create(tweet_id: self.id, user_id: user_id)
+  end
+
+
 end
