6285a7d5ba4407e6108cb0110fd358ab7aece5e7
sofia <sofiaalvaradohenriquez@gmail.com>
2023-09-27 08:09:38 -0600

    Adding Quoting & Quotes count

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,45 +1,33 @@
 class Tweet < ApplicationRecord
   belongs_to :user
-  #self reference
+  #self references
   has_many :retweets, class_name: "Tweet", foreign_key: :retweet_id
+  has_many :quotes, class_name: "Tweet", foreign_key: :quote_id
   #a bookmark belongs to a single Tweet
   has_many :bookmarks
   has_many :hashtags
   has_many :likes
-
   #Validating if content exists and if in a length betweent 1 and 255
   #If content is not provided throw message: is too short (minimum is 1 character)", "Please enter your text"
   validates :content, length: { within:(1...255)}, presence: { message: "Please enter your text" }
   validates :quote, length: { maximum: 225 }
   validates :content, presence: true
   validates_associated :user, :retweets
-  #validates :content, presence: true, on: :create_retweet, if: -> { content.present? }
-
- 
 
   #Retweets counts: Create a new scope that retrieves the number of retweets
   scope :retweets_count, -> (tweet_id){ where(retweet_id: tweet_id).count }
+  scope :quotes_count, -> (tweet_id){ where(quote_id: tweet_id).count }
   
   #Retweet method: Create a method that encapsulates the retweet logic accepting a user a parameter
   def create_retweet(user_id)
     original_tweet = Tweet.find(self.id) # Find the original tweet
     retweet_content = original_tweet.content # Access the content of the original tweet
     retweet = Tweet.create(user_id: user_id, content: retweet_content, retweet_id: self.id)
-    retweet
   end
-
+  #Create a method that encapsulates the retweet logic accepting a user an a text body as parameter
   def create_quoted_retweet(user_id, quote)
     original_tweet = Tweet.find(self.id) # Find the original tweet
     tweet_content = original_tweet.content # Access the content of the original tweet
-
-    #user = User.find(user_id)
-
     quoted_retweet = Tweet.create(user_id: user_id, content: tweet_content, quote_id: self.id, quote: quote)
-    quoted_retweet 
   end
 end
-
-
-# Example usage:
-# user = User.find(1) # Replace with the actual user you're interested in
-# user_tweets = user.tweets_by_user(user.id)
