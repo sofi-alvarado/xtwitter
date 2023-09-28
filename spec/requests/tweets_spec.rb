ad25619b8476399105c8268f5812f9be3c46e406
sofia <sofiaalvaradohenriquez@gmail.com>
2023-09-28 16:00:20 -0600

    Adding json schema

--- spec/requests/tweets_spec.rb
+++ spec/requests/tweets_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe "::TweetsController", type: :request do
- 
-
+  describe "GET /tweets" do
+    it "works! (now write some real specs)" do
+      get _tweets_index_path
+      expect(response).to have_http_status(200)
+    end
+  end
 end
