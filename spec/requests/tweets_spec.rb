9ef3b56434504cb0bd7c614441a64f37bfca449c
sofia <alvaradosofiah@outlook.com>
2023-09-28 23:04:36 -0600

    Adding request specs

--- spec/requests/tweets_spec.rb
+++ spec/requests/tweets_spec.rb
@@ -1,10 +1,95 @@
 require 'rails_helper'
 
-RSpec.describe "::TweetsController", type: :request do
-  describe "GET /tweets" do
-    it "works! (now write some real specs)" do
-      get _tweets_index_path
+RSpec.describe "Tweets", type: :request do
+  describe "GET /" do
+    it "returns a successful response" do
+      get root_path
       expect(response).to have_http_status(200)
     end
   end
+
+  describe "POST /tweets" do
+    it "creates a new tweet" do
+      post create_tweet_path
+      expect(response).to have_http_status(302) # Assuming you redirect after creating a tweet
+    end
+  end
+
+  describe "PATCH /tweets/:id" do
+    it "updates an existing tweet" do
+      tweet = create(:tweet)
+      patch update_tweet_path(tweet)
+      expect(response).to have_http_status(302)
+    end
+  end
+
+  describe "POST /tweets/:id/like" do
+    it "likes an existing tweet" do
+      tweet = create(:tweet)
+      post likes_tweet_path(tweet)
+      expect(response).to have_http_status(302)
+  end
+
+  describe "DELETE /tweets/:id/unlike" do
+    it "unlikes an existing tweet" do
+      tweet = create(:tweet)
+      delete unlikes_tweet_path(tweet)
+      expect(response).to have_http_status(302)
+    end
+  end
+
+  describe "POST /tweets/:id/retweet" do
+    it "retweets an existing tweet" do
+      tweet = create(:tweet)
+      post retweets_path(tweet)
+      expect(response).to have_http_status(302)
+    end
+  end
+
+  describe "POST /tweets/:id/quote" do
+    it "quotes an existing tweet" do
+      tweet = create(:tweet)
+      post quotes_path(tweet)
+      expect(response).to have_http_status(302)
+    end
+  end
+
+  describe "POST /tweets/:id/reply" do
+    it "replies to an existing tweet" do
+      tweet = create(:tweet)
+      post reply_tweets_path(tweet)
+      expect(response).to have_http_status(302)
+    end
+  end
+
+  describe "POST /tweets/:id/bookmark" do
+    it "bookmarks an existing tweet" do
+      tweet = create(:tweet)
+      post bookmark_tweets_path(tweet)
+      expect(response).to have_http_status(302)
+    end
+  end
+
+  describe "GET /user_tweets" do
+    it "returns a successful response" do
+      get user_tweets_path
+      expect(response).to have_http_status(200)
+    end
+  end
+
+  describe "GET /user_tweets_and_replies" do
+    it "returns a successful response" do
+      get tweets_and_replies_path
+      expect(response).to have_http_status(200)
+    end
+  end
+
+  describe "GET /tweets/:id/stats" do
+    it "returns a successful response" do
+      tweet = create(:tweet)
+      get stats_tweets_path(tweet)
+      expect(response).to have_http_status(200)
+    end
+  end
+end
 end
