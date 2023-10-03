fa3eb10349166a731f3c37afc3c38d9c5d9e298c
sofia <sofiaalvaradohenriquez@gmail.com>
2023-10-02 21:48:36 -0600

    Adding relationship test to Taggings

--- app/models/tagging.rb
+++ app/models/tagging.rb
@@ -1,4 +1,4 @@
 class Tagging < ApplicationRecord
-  belongs_to :tweets, class_name: "Tweet"
-  belongs_to :hashtags, class_name: "Hashtag"
+  belongs_to :tweets, class_name: "Tweet", foreign_key: "tweet_id"
+  belongs_to :hashtags, class_name: "Hashtag", foreign_key: "hashtag_id"
 end
--- spec/models/tagging_spec.rb
+++ spec/models/tagging_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Tagging, type: :model do
-  
+    describe "associations" do
+        it { should belong_to(:tweets).class_name("Tweet").with_foreign_key(:tweet_id) }
+        it { should belong_to(:hashtags).class_name("Hashtag").with_foreign_key(:hashtag_id) }
+      end
 end
